6a52960b9f2738dc45e1401aa29cb1078830ec03
ivancollazoo <collazoivan1412@gmail.com>
2025-01-31 16:45:58 -0400

    Last Commit of the HW

---
+++ src/demo1/DemoTest1.java
@@ -0,0 +1,16 @@
+package demo1;
+
+public class DemoTest1 {
+	public static void main(String[] args) {
+
+		int a = 7675;
+		int b = 7685;
+
+		if (a < b) {
+			System.out.println("a is bigger");
+		} else {
+			System.out.println("False");
+		}
+	}
+
+}
